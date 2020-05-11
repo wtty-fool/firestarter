2f3b7351d51ede3dbdaed0d8d95c35e534fcac84
wtty-fool <wtty.fool@gmail.com>
2020-05-11 13:27:29 +0200

    initialize code

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	prometheus.MustRegister(prometheus.NewBuildInfoCollector())
+}
+
+func main() {
+	http.Handle("/metrics", promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{
+			EnableOpenMetrics: true,
+		},
+	))
+	log.Fatal(http.ListenAndServe(":9100", nil))
+}
